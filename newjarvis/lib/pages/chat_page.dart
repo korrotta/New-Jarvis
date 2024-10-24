66bea94f0a57720b6475bb884ce698ce9f6a9acb
korrotta <hoaihieulam@gmail.com>
2024-10-24 20:47:30 +0700

    Fix some paddings

--- newjarvis/lib/pages/chat_page.dart
+++ newjarvis/lib/pages/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:newjarvis/components/bottom_nav_section.dart';
 import 'package:newjarvis/components/end_drawer_section.dart';
