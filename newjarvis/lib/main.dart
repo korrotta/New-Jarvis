196eac68d7ab16020369ab1a94befe69286abbf6
XuanHoa2307 <xuanhoa711@gmail.com>
2024-12-30 01:20:56 +0700

    upcode knowledgebase_knowledge-and-knowledge-dataSource

--- newjarvis/lib/main.dart
+++ newjarvis/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:newjarvis/components/route_controller.dart';
 import 'package:newjarvis/firebase_options.dart';
 import 'package:newjarvis/providers/auth_provider.dart';
+import 'package:newjarvis/providers/knowledge_base_provider.dart';
+import 'package:newjarvis/providers/knowledge_base_unit_provider.dart';
 import 'package:newjarvis/states/category_state.dart';
 import 'package:newjarvis/states/chat_state.dart';
 import 'package:newjarvis/states/prompts_state.dart';
@@ -28,6 +30,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => ChatState()),
         ChangeNotifierProvider(create: (context) => PromptState()),
         ChangeNotifierProvider(create: (context) => CategoryState()),
+        ChangeNotifierProvider(create: (context) => KnowledgeBaseProvider()),
+        ChangeNotifierProvider(create: (context) => UnitProvider()),
+
       ],
       child: MaterialApp(
         title: 'New Jarvis',
